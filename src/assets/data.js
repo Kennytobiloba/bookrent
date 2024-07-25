3587ce16036ed432a1ea023a02c67b9d2d187479
Kennytobiloba <125762815+Kennytobiloba@users.noreply.github.com>
2024-07-25 13:44:21 +0100

    t

--- src/assets/data.js
+++ src/assets/data.js
@@ -273,7 +273,7 @@ const data = [
         "autor":"J.R.R. Tolkien",
         "title": "The Final Gambit",
         "description": "When Branch’s brother, Floyd, is kidnapped for his musical talents by a pair of nefarious pop-star villains, Branch and Poppy embark on a harrowing and emotional journey to reunite the other brothers and rescue Floyd from a fate even worse than pop-culture obscurity.",
-        "genre": "Action/Adventure/Sci-fi",
+        "genre": "Action/Adventure/Sci-fi", 
         "rating": 5,
         "price": 100,
         "createdAt": "2024-04-19",
